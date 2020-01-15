de2c49e68c1684aa8e16e43b18fbd29db3ae6013
Nathaniel Stenzel <nathanielstenzel@users.noreply.github.com>
2020-01-15 00:05:59 -0600

    Create check_max_extrusion_speed.py

---
+++ check_max_extrusion_speed.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python
+#Written by Nathaniel J Stenzel on January 14, 2020
+"""
+This program will help you find out just how fast your filament gets pushed in a particular gcode file_name
+If you set print_speed in the parameter list, this program will instead tell you just how fast you could push it given x_speed, y_speed, print_speed and extrusion_speed.
+"""
+file_name = ""
+import math,sys
+line = ""
+last_extrusion = last_x = last_y = extrusion = x = y = 0
+max_movement_speed = max_extrusion_speed = 0 #start tracking the fastest later
+print_speed = None
+x_speed = y_speed = extrusion_speed = move_speed = 999999
+params = {}
+debug = False
+for param in ["x_speed","y_speed","extrusion_speed","print_speed","debug"]:
+    params[param] = locals()[param]
+if len(sys.argv) < 2 or "help" in sys.argv or "-h" in sys.argv or sys.argv[1].count("="):
+    print "Usage:"
+    print "\tcheck_max_extrusion_speed.py filename [x_speed=#] [y_speed=#] [print_speed=#] [extrusion_speed=#] [debug=True/False]"
+    print "Setting the print speed parameter will override the movement speed defined in the gcode."
+    print "These speeds are in mm/s."
+    exit()
+file_name = sys.argv[1]
+for arg in sys.argv[2:]:
+    exec(arg,{},params)
+gcode = open(file_name,"r")
+locals().update(params)
+if print_speed !=None:
+    move_speed = print_speed
+retract_count = 0
+#add something to consider more printing areas like infill and support and inner and outer wall or whatever the slicer gives us a clue to
+line_number = 0
+while str(line).upper() .split(";")[0]<> "M107":
+    line = gcode.readline().strip().upper()
+    line_number += 1
+while line:
+    line = line.split(";")[0]
+    line = line.split()
+    if line and line[0] in ("G0","G1"):
+        last_x = x
+        last_y = y
+        last_extrusion = extrusion
+        for part in line:
+            if part[0] == "X":
+                x = float(part[1:])
+            elif part[0] == "Y":
+                y = float(part[1:])
+            elif part[0] == "E":
+                extrusion = float(part[1:])
+            elif part[0] == "F" and print_speed == None: #feed rate in mm/min
+                move_speed = float(part[1:])/60
+        extrusion_delta = extrusion-last_extrusion
+        #basic changes in x,y
+        x_delta = abs(x-last_x)
+        y_delta = abs(y-last_y)
+        if extrusion_delta < 0:
+            retract_count += 1
+            #print line, "from",last_extrusion
+        if extrusion_delta > 0 and (x_delta > 0 or y_delta > 0): #we do not care about retractions
+            move = math.hypot(x_delta,y_delta)
+            #find out the speed based on x_speed and y_speed
+            scaled_x = x_delta / x_speed
+            scaled_y = y_delta / y_speed
+            scaled_move = move / move_speed
+            scaled_extrusion = extrusion_delta / extrusion_speed
+            max_scale = 0
+            speed_limit = ""
+            #I am doing something totally wrong here and need to think this through.
+            if debug:
+                if scaled_x > max_scale:
+                    speed_limit = "X"
+                if scaled_y > max_scale:
+                    speed_limit = "Y"
+                if scaled_move > max_scale:
+                    speed_limit = "move"
+                if scaled_extrusion > max_scale:
+                    speed_limit = "E"
+            max_scale = max(scaled_x,scaled_y,scaled_move,scaled_extrusion)
+            this_extrusion_speed = extrusion_delta/max_scale
+            max_extrusion_speed = max(this_extrusion_speed,max_extrusion_speed)
+            max_movement_speed = max(move/max_scale, max_movement_speed)
+            #print scaled_x,scaled_y,scaled_move,scaled_extrusion,"\t",scaled_x,scaled_y,scaled_move,scaled_extrusion
+            if debug:
+                print max_scale, speed_limit, this_extrusion_speed, x_delta/max_scale, y_delta/max_scale, move/max_scale
+    line = gcode.readline().strip().upper()
+    line_number += 1
+print "Maximum extrusion speed:",max_extrusion_speed
+print "Maximum movement speed:",max_movement_speed
+print "Retraction count:",retract_count
+print "Line count:",line_number
